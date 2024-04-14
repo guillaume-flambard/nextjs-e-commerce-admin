ed25452d602f65dd1fbcf31ebe0abf992f993ae3
Guillaume Flambard <g.flambard@gmail.com>
2024-04-14 15:47:09 +0200

    added/switcher-Store

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,5 +1,22 @@
-export default function DashboardPage() {
+import prismadb from "@/lib/prismadb"
+
+interface DashboardPageProps {
+    params: {
+        storeId: string
+    }
+}
+
+const DashboardPage: React.FC<DashboardPageProps> = async ({
+    params
+}) => {
+    const store = await prismadb.store.findFirst({
+        where: {
+            id: params.storeId
+        }
+    })
     return (
-        <div>DashboardPage</div>
+        <div>Active store : {store?.name}</div>
     )
 }
+
+export default DashboardPage
--- providers/modal-provider.tsx
+++ providers/modal-provider.tsx
@@ -4,15 +4,15 @@ import StoreModal from "@/components/ui/modals/store-modal";
 import { useEffect, useState } from "react";
 
 export const ModalProvider = () => {
-    const [isMounted, setIsMounted] = useState(false);
+  const [isMounted, setIsMounted] = useState(false);
 
-    useEffect(() => {
-        setIsMounted(true);
-    }, []);
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
 
-    if (!isMounted) {
-        return null;
-    }
+  if (!isMounted) {
+    return null;
+  }
 
-    return <StoreModal />
-}
+  return <StoreModal />;
+};
